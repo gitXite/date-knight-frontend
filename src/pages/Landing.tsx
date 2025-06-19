49c376a8e05d00fd0e65a6e65e3b8fc2d4526f9e
Daniel <Daniel.halaas@gmail.com>
2025-06-19 16:51:58 +0200

    Update and rename LandingPage.tsx to Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,13 @@
 import dateKnightLogo from '../assets/images/dateKnightLogo.png';
 
-function LandingPage() {
+function Landing() {
     return (
         <div className='flex flex-col justify-center items-center h-screen w-screen'>
             <div className='flex flex-col items-center w-2/4'>
                 <img src={dateKnightLogo} className='w-2/4'/>
             </div>
         </div>
-    )
+    );
 }
 
-export default LandingPage;
+export default Landing;
