e48db47e7bd5750f17dff0440a7ddf0bc34212ec
Daniel <Daniel.halaas@gmail.com>
2025-06-19 17:13:47 +0200

    Setting up landing page animations

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 import dateKnightLogo from '../assets/images/dateKnightLogo.png';
 
-function Landing() {
+function Landing({ onCTAClick }) {
+    const [animateLogo, setAnimateLogo] = useState(false);
+    const navigate = useNavigate();
+
+    const handleCTAClick = () => {
+        setAnimateLogo(true);
+
+        setTimeout(() => {
+            onCTAClick();
+            navigate('/start');
+        }, 500);
+    };
+    
     return (
         <div className='flex flex-col justify-center items-center h-screen w-screen'>
-            <div className='flex flex-col items-center w-2/4'>
-                <img src={dateKnightLogo} className='w-2/4'/>
-            </div>
+            {animateLogo ? (
+                <div>
+                    <div>
+{/*                         Insert animated logo with framer motion */}
+                    </div>
+                    <div>
+{/*                         Insert animation for button fade-out */}
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <div className='flex flex-col items-center w-2/4'>
+                        <img src={dateKnightLogo} className='w-2/4'/>
+                    </div>
+                    <div>
+{/*                         Insert CTA button component */}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
