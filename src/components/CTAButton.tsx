75295e4e6af4c861bd55b7fc7441bef2c0ed1c53
Daniel <Daniel.halaas@gmail.com>
2025-06-19 20:03:11 +0200

    Button styling

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -3,7 +3,7 @@ export type CTAButtonProps = {
 };
 
 function CTAButton({ onClick }: CTAButtonProps) {
-    return <button onClick={onClick}>Start</button>;
+    return <button onClick={onClick} className='bg-gray-800 text-white dark:bg-white hover:bg-gray-100 dark:text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>Start</button>;
 }
 
 
