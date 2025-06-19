c640f3a922a061e1c1f6ec456d4f49953fa157d9
Daniel <Daniel.halaas@gmail.com>
2025-06-19 18:09:20 +0200

    Refactored into layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Layout from './layouts/Layout';
 import Header from './components/Header';
 import Landing from './pages/Landing';
 
@@ -7,10 +8,7 @@ import Landing from './pages/Landing';
 function App() {
     return (
         <Router>
-            <Header />
-            <Routes>
-                <Route path='/' element={<Landing />} />
-            </Routes>
+            <Layout />
         </Router>
     );
 }
