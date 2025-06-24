67adb4062aab2dc15fc76930719ee5672af91c3a
Daniel <Daniel.halaas@gmail.com>
2025-06-24 09:15:44 +0200

    CTAButton component styling

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -3,7 +3,7 @@ export type CTAButtonProps = {
 };
 
 function CTAButton({ onClick }: CTAButtonProps) {
-    return <button onClick={onClick}>Start</button>;
+    return <button onClick={onClick} className='flex items-center h-10 w-25 rounded-lg text-white bg-rose-900 text-xl cursor-pointer drop-shadow-md'>Start</button>;
 }
 
 
